6cd41accd91f7ae494269d58ff6058d3988112aa
Hugo David Nogueda Hernández <101530252+HugoCDD@users.noreply.github.com>
2022-04-29 14:43:43 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+let numeros=[1,2,3,4,5,6,7,8,9];
+        let x=0;
+        let y=1;
+        let z=2;
+        for(i=0;i<3;i++){
+            console.log(numeros[x],numeros[y], numeros[z]);
+            x=x+3;
+            y=y+3;
+            z=z+3;
+        }
+        console.log(" ");
+        let j=8;
+        let l=7;
+        let k=6;
+        for(i=0;i<3;i++){
+            console.log(numeros[j],numeros[l], numeros[k]);
+            j=j-3;
+            l=l-3;
+            k=k-3;
+        }
+        console.log(" ");
+        let a=0;
+        let b=3;
+        let c=6;
+        for(i=0;i<3;i++){
+            console.log(numeros[a],numeros[b], numeros[c]);
+            a++;
+            b++;
+            c++;
+        }
+        console.log(" ");
+        let n=8;
+        let m=5;
+        let v=2;
+        for(i=0;i<3;i++){
+            console.log(numeros[n],numeros[m], numeros[v]);
+            n=n-1;
+            m=m-1;
+            v=v-1;
+        }
